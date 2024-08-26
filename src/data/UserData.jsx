0b40cd8301d00b077681200a3ab4e32a23d6a34f
Rahul <Rahul2.gandhi@ril.com>
2024-08-26 20:29:56 +0530

    files commit

---
+++ src/data/UserData.jsx
@@ -0,0 +1,30 @@
+export const UserData = {
+  name: "Rahul Gandhi",
+  about : "I'm a front end developer that learned everything on my own. I specialize in creating dynamic, accessible websites that work on any device. Furthermore, I regularly create creative solutions that improve user experiences. I am proficient in React.js, Redux, JavaScript, HTML5, and CSS. I guarantee efficient code management and communication thanks to my knowledge with Git version control. Likewise, I'm also skilled at developing apps using mini program studio frameworks and exceeding client expectations with my high-caliber solutions.",
+  resumeUrl : "https://drive.google.com/file/d/1LSVLVmJA_3fhWtZPc_d_pHnkXhL4s-Oc/view",
+  FooterLink : "https://www.linkedin.com/in/rahulvijay81/",
+  socialMedia: [
+    {
+      socialMediaName: "github",
+      url: "https://github.com/rahulvijay81",
+      icon: "AiFillGithub",
+    },
+    {
+      socialMediaName: "linkedin",
+      url: "https://www.linkedin.com/in/rahulvijay81/",
+      icon: "FaLinkedinIn",
+    },
+  ],
+  typewriterOptions: {
+    strings: [
+      "Front End Developer",
+      "React Js Developer",
+      "React Native Developer",
+      "UI Developer",
+      "Javascript Developer",
+    ],
+    autoStart: true,
+    loop: true,
+    deleteSpeed: 30,
+  },
+};
